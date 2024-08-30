3ae201d565e6f83036dc22cd68905c900875e543
SlavikBadBoy <99145820+SlavikBadBoy@users.noreply.github.com>
2024-08-30 18:08:37 +0000

    Deploying to gh-pages from @ SlavikBadBoy/coding-magic@6c519400a433b80b1ae467f6727aee8278a5459d 🚀

---
+++ index.5edf45eb.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){var n;document.querySelector("#main").innerHTML=n.map((function(n){return'<section class=\'section\'>\n      <div class="container">\n        <div class="game '.concat(n.class,'"></div>\n      </div>\n    </section>')})).join("")}));
+//# sourceMappingURL=index.5edf45eb.js.map
