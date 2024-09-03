f613689a768e96ed1c082d5b2de2e710a5c6811f
sheludkovladyslav <128414119+sheludkovladyslav@users.noreply.github.com>
2024-09-03 23:19:48 +0000

    Deploying to gh-pages from @ SlavikBadBoy/coding-magic@d475e8e69d9236e4bcd0d29661addd40a4622c05 🚀

--- index.5edf45eb.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",(function(){var n;document.querySelector("#main").innerHTML=n.map((function(n){return'<section class=\'section\'>\n      <div class="container">\n        <div class="game '.concat(n.class,'"></div>\n      </div>\n    </section>')})).join("")}));
-//# sourceMappingURL=index.5edf45eb.js.map
--- index.abd6a5d6.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",(()=>{var n;document.querySelector("#main").innerHTML=n.map((n=>`<section class='section'>\n      <div class="container">\n        <div class="game ${n.class}"></div>\n      </div>\n    </section>`)).join("")}));
-//# sourceMappingURL=index.abd6a5d6.js.map
