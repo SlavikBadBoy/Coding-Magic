6649093b63411038f7f539e9f2138002e2ba22c7
Vladyslav Sheludko <vladsh2103@gmail.com>
2024-09-03 22:06:36 -0400

    Зробив повністю html, стилі, логіку модалки, тріхи змінив global та додав ще колір у variables, додав дата-елементи в хедері

---
+++ src/js/greeting-modal.js
@@ -0,0 +1,34 @@
+const backdrop = document.querySelector('[data-backdrop]');
+const modal = document.querySelector('[data-modal]');
+
+const closeModalHandler = event => {
+  if (event.target.closest('[data-modal-close]')) {
+    backdrop.classList.add('is-hidden');
+    modal.removeEventListener('click', closeModalHandler); // Спочатку видаляємо обробник події
+    setTimeout(() => {
+      backdrop.remove();
+    }, 250);
+  }
+};
+
+modal.addEventListener('click', closeModalHandler);
+
+const form = document.querySelector('[data-modal-form]');
+const input = document.querySelector('[data-modal-input]');
+const output = document.querySelector('[data-modal-output]');
+const modalFormHandler = event => {
+  event.preventDefault();
+  let inputValue = input.value.trim();
+
+  if (inputValue.length > 0) {
+    output.innerHTML = `Вітаємо, ${inputValue}!`;
+
+    backdrop.classList.add('is-hidden');
+    modal.removeEventListener('click', closeModalHandler); // Спочатку видаляємо обробник події
+    setTimeout(() => {
+      backdrop.remove();
+    }, 250);
+  } else alert("Введіть ваше ім'я!");
+};
+
+form.addEventListener('submit', modalFormHandler);
