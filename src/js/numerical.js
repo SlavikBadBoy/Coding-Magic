e59b74e84603fd05bb60f22a09d23a2528e49720
maxvojcuk <maksimvojcuk09@gmail.com>
2024-09-07 20:31:23 +0300

    готово

---
+++ src/js/numerical.js
@@ -0,0 +1,35 @@
+window.addEventListener('load', () => {
+  document.querySelector(
+    '.guess-number'
+  ).innerHTML = `<form action="#" class="form__numerical">
+          <input
+            class="input__numerical"
+            type="number"
+            id="number"
+            placeholder="Введіть число"
+          />
+          <button class="button__numerical" id="checkNumber"></button>
+        </form>
+
+        <p class="result__numerical" id="result"></p>
+`;
+  const number = document.querySelector('#number');
+  const buttonCheckNumber = document.querySelector('#checkNumber');
+  const result = document.querySelector('#result');
+  buttonCheckNumber.addEventListener('click', checkNumber);
+});
+
+let randomNumber = Math.floor(Math.random() * 10) + 1;
+
+const checkNumber = () => {
+  const userGuess = parseInt(number.value);
+
+  if (userGuess === randomNumber) {
+    result.innerHTML = `Вітаю, ви вгадали число! ${randomNumber}`;
+    result.style.color = 'green';
+  } else if (userGuess < randomNumber) {
+    result.innerHTML = 'Ви не вгадали';
+    result.style.color = 'red';
+  }
+  randomNumber = Math.floor(Math.random() * 10) + 1;
+};
