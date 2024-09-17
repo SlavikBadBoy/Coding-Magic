447c048396623cdcb5010b027c323df96e977103
Vladyslav Sheludko <vladsh2103@gmail.com>
2024-09-16 22:54:48 -0400

    повністю доробив динозавра

--- src/js/google__dinosaur.js
+++ src/js/google__dinosaur.js
@@ -1,18 +1,25 @@
 class GoogleDinosaur {
   constructor() {
     this.dino = document.querySelector('.dino');
-    this.gameArea = document.querySelector('.google-dinosaur');
+    this.gameArea = document.querySelector('.game__container');
     this.gameOver = false;
     this.isJumping = false;
     this.init();
   }
+
   init() {
     document.addEventListener('keydown', event => {
-      if (event.code === 'Space' && !this.isJumping) {
+      if (event.code === 'Space' && !this.isJumping && !this.gameOver) {
         this.jump();
       }
-      this.renderObstacles();
     });
+    document.addEventListener('mousedown', event => {
+      if (!this.isJumping && !this.gameOver) {
+        this.jump();
+      }
+    });
+    this.renderObstacles();
+    this.checkCollision();
   }
 
   jump() {
@@ -25,16 +32,76 @@ class GoogleDinosaur {
     }, 500);
   }
 
-  collision() {
-    // зіткнення
+  checkCollision() {
+    const gameProcess = () => {
+      if (this.gameOver === true) return;
+      const obstacle = this.gameArea.querySelector('.cactus');
+      const obstacleRect = obstacle.getBoundingClientRect();
+      const dinoRect = this.dino.getBoundingClientRect();
+      if (
+        dinoRect.right > obstacleRect.left &&
+        dinoRect.left < obstacleRect.right &&
+        dinoRect.bottom > obstacleRect.top
+      ) {
+        this.endGame(obstacle);
+        return;
+      }
+
+      requestAnimationFrame(gameProcess);
+    };
+    requestAnimationFrame(gameProcess);
   }
 
   renderObstacles() {
-    // рендер перешкод
+    const obstacle = document.createElement('div');
+    obstacle.classList.add('cactus');
+    obstacle.style.height = this.randomHeight();
+    this.gameArea.appendChild(obstacle);
+
+    const animationHandler = () => {
+      obstacle.removeEventListener('animationiteration', animationHandler);
+      this.gameArea.removeChild(obstacle);
+      if (!this.gameOver) {
+        this.renderObstacles();
+      }
+    };
+    obstacle.addEventListener('animationiteration', animationHandler);
   }
 
-  endGame() {
-    // кінець гри
+  randomHeight() {
+    return `${Math.random() * (70 - 40) + 40}px`;
+  }
+
+  endGame(obstacle) {
+    obstacle.style.animationPlayState = 'paused';
+    this.gameOver = true;
+    this.gameOverModal();
+  }
+
+  gameOverModal() {
+    const modalHtml = `
+    <div class="dinosaur__modal">
+      <p class="dinosaur__lose">Ви програли!</p>
+      <button class="dinosaur__restart button">Спробувати знову</button>
+    </div>
+    `;
+    this.gameArea.insertAdjacentHTML('beforeend', modalHtml);
+
+    const modal = this.gameArea.querySelector('.dinosaur__modal');
+    setTimeout(() => {
+      modal.classList.add('show');
+    }, 10);
+
+    const retryButton = this.gameArea.querySelector('.dinosaur__restart');
+    retryButton.addEventListener('click', () => this.restartGame());
+  }
+
+  restartGame() {
+    this.gameArea.querySelector('.dinosaur__modal').remove();
+    this.gameArea.querySelector('.cactus').remove();
+    this.gameOver = false;
+    this.renderObstacles();
+    this.checkCollision();
   }
 }
 
