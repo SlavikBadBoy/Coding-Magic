11cb68b37b9f8565641ad62d392bc9b1fdd269c9
maxvojcuk <maksimvojcuk09@gmail.com>
2024-09-22 21:11:52 +0300

    перевірка

--- src/js/our-team.js
+++ src/js/our-team.js
@@ -1,12 +1,10 @@
+import team from './data/team';
+
 export const ourTeamInit = () => {
   const teamList = document.querySelector('.our-team');
-  const team = [
-    { img: './...', name: 'Учасник 1', temmeteInfo: 'Про учасника 1' },
-    { img: './...', name: 'Учасник 1', temmeteInfo: 'Про учасника 2' },
-    { img: './...', name: 'Учасник 1', temmeteInfo: 'Про учасника 3' },
-  ];
   teamList.innerHTML = `  
-      <h2>Наша команда</h2>
+      <h2 class="title__our-team">Наша команда</h2>
+      <button class="left-button" data-controls="left"></button>
       <div class="team-slider">
         <ul class="team-list">
         ${team
@@ -14,18 +12,32 @@ export const ourTeamInit = () => {
             teammate =>
               `<li class="team-item">
                     <article>
-                        <picture></picture>
-                        <p>${teammate.name}</p>
-                        <p>${teammate.temmeteInfo}</p>
+                        <picture class="footo__teamate">${teammate.foto}</picture>
+                        <p class="name__teammate">${teammate.name}</p>
+                        <p class="info__teammate">${teammate.temmeteInfo}</p>
                     </article>
                 </li>`
           )
           .join('')}
           </ul>
       </div>
-      <button class="controls-left" data-controls="left">left</button>
-      <button class="controls-right" data-controls="right">right</button>
+      
+      <button class="right-button" data-controls="right"></button>
       <ul class="pagination">
     </ul>
     `;
+  const slider = document.querySelector('.team-list');
+  const slidesCount = document.querySelectorAll('.team-item').length;
+  const paginetion = document.querySelector('.pagination');
+  for (let i = 0; i < slidesCount; i++) {
+    const dot = document.createElement('li');
+    dot.classList.add('dot');
+    if (i === 0) dot.classList.add('active');
+    paginetion.appendChild(dot);
+  }
+  let indexs = 0;
+  const dots = document.querySelector('.dot');
 };
+window.addEventListener('load', () => {
+  ourTeamInit();
+});
