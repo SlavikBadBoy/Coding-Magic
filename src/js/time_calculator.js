6ab04e762e2f65d1c736ce2ba916180c10b50416
SlavikBadBoy <buracenkoslavik@gmail.com>
2024-09-25 15:41:06 +0300

    Виправив помилки

--- src/js/time_calculator.js
+++ src/js/time_calculator.js
@@ -3,14 +3,13 @@ export const timeCalculatorInit = () => {
   timeCalculatorContainer.innerHTML = `
   <section class="time-calculator"></section>
   <div class="time-calculator__container">
-    <h1 class="time-calculator__title">Калькулятор часу</h1>
+    <h1 class="time-calculator__title title">Калькулятор часу</h1>
     <div class="time-calculator__formcontainer">
     <div class="time-calculator__form">
       <input class="time-calculator__input" type="number" placeholder="Введіть число" id="timeCalculatorInput"/>
       <button class="time-calculator__button" id="timeCalculatorBtn"></button>
     </div>
-      
-      <p class="time-calculator_result" id="timeCalculatorResult">Відповідь</p>
+      <p class="time-calculator_result" id="timeCalculatorResult"></p>
     </div>
   </div>
 </section>`;
@@ -25,12 +24,9 @@ export const timeCalculatorInit = () => {
     const hours = Math.floor(numberHours % 24);
     const minutes = Math.floor(summaryTime % 60);
 
- 
     timeCalculatorResult.textContent = `${days} дн. ${hours}:${minutes}`;
   };
   timeCalculatorBtn.addEventListener('click', convertMinutes);
 };
 
-window.addEventListener('load', () => {
-  timeCalculatorInit(); //Видалити
-});
+
